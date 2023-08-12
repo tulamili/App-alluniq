1bd169126a9fcfaf4420f357bd864b203dac8493
Toshiyuki Shimono <bin4tsv@gmail.com>
2023-08-12 14:25:22 +0900

    alluniq 関連で必要なファイルを用意。

---
+++ alluniq.pm
@@ -0,0 +1,37 @@
+package App::alluniq ;  
+our $VERSION = '0.250' ; 
+our $DATE = '2023-08-12T14:40+09:00' ; 
+
+=encoding utf8
+
+=head1 NAME
+
+App::alluniq -- Align the number of columns on each line upon TSV input.
+
+=head1 SYNOPSIS
+
+This module provides a Unix-like command `F<alluniq>'. 
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+   App::csel
+   App::colgrep
+   App::colsummary
+   App::collen (to be published, soon after Aug 2023)
+
+=head1 AUTHOR
+
+下野寿之 Toshiyuki SHIMONO <bin4tsv@gmail.com> The Institute of Statistical Mathematics, a visiting researcher. 
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2015-2023 Toshiyuki SHIMONO. All rights reserved.
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
+
+1 ;
